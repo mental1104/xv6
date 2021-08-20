aec44e5ee3175b68da74c2e56d007ed903443193
mental1104 <mental1104@gmail.com>
2021-08-20 23:45:13 +0800

    lab3 Incomplete

--- kernel/defs.h
+++ kernel/defs.h
@@ -178,6 +178,12 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+void            vmwalk(pagetable_t pagetable, int depth);
+void            vmprint(pagetable_t pagetable);
+void            kvmmapkern(pagetable_t pagetable, uint64 va, uint64 pa, uint64 sz, int perm);
+pagetable_t     kvmcreate();
+void            kvmfree(pagetable_t kpagetale) ;
+void            u2kvmcopy(pagetable_t pagetable, pagetable_t kpagetable, uint64 oldsz, uint64 newsz);
 
 // plic.c
 void            plicinit(void);
