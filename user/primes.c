114a3f727d9c419e14d5443bdd6568cf0233e05e
ChatGPT <41898282+github-actions[bot]@users.noreply.github.com>
2026-07-14 16:12:27 +0000

    fix: make primes pipeline iterative

--- user/primes.c
+++ user/primes.c
@@ -1,59 +1,81 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-void children_process(int p[2]){
+
+static void
+run_pipeline(int input[2])
+{
+  int read_fd = input[0];
+  close(input[1]);
+
+  for(;;){
     int prime;
-    int pp[2];
-    int num;
-    int len;//to figure out the return val of read;
-
-    close(p[1]);
-    len = read(p[0],&prime,sizeof(int));
-    if(len == 0){
-        close(p[0]);
-        exit(0);
+    if(read(read_fd, &prime, sizeof(prime)) != sizeof(prime)){
+      close(read_fd);
+      exit(0);
     }
+
     printf("prime %d\n", prime);
-    pipe(pp);
 
-    if(fork()==0){
-        close(p[0]);
-        children_process(pp);
-    }else{
-        close(pp[0]);
-        while(1){
-            len = read(p[0],&num,sizeof(int));
-            if(len == 0)
-                break;
-            
-            if(num%prime !=0)
-                write(pp[1],&num,sizeof(int));
-        }
-        close(p[0]);
-        close(pp[1]);
-        wait(0);
+    int next[2];
+    if(pipe(next) < 0){
+      close(read_fd);
+      exit(1);
     }
+
+    int pid = fork();
+    if(pid < 0){
+      close(read_fd);
+      close(next[0]);
+      close(next[1]);
+      exit(1);
+    }
+
+    if(pid == 0){
+      close(read_fd);
+      close(next[1]);
+      read_fd = next[0];
+      continue;
+    }
+
+    close(next[0]);
+
+    int num;
+    while(read(read_fd, &num, sizeof(num)) == sizeof(num)){
+      if(num % prime != 0)
+        write(next[1], &num, sizeof(num));
+    }
+
+    close(read_fd);
+    close(next[1]);
+    wait(0);
     exit(0);
+  }
 }
 
-int main(int argc, char** argv){
-    if(argc>1){
-        fprintf(2, "Usage: primes");
-        exit(1);
-    }
+int
+main(int argc, char **argv)
+{
+  if(argc != 1){
+    fprintf(2, "usage: primes\n");
+    exit(1);
+  }
 
-    int i;
-    int p[2];
-    pipe(p);
+  int input[2];
+  if(pipe(input) < 0)
+    exit(1);
 
-    if(fork()==0){
-        children_process(p);
-    }else{
-        close(p[0]);
-        for(i = 2;i<=35;i++)
-            write(p[1],&i,sizeof(int));
-        close(p[1]);
-        wait(0);
-    }
-    exit(0);
+  int pid = fork();
+  if(pid < 0)
+    exit(1);
+
+  if(pid == 0)
+    run_pipeline(input);
+
+  close(input[0]);
+  for(int i = 2; i <= 35; i++)
+    write(input[1], &i, sizeof(i));
+  close(input[1]);
+  wait(0);
+  exit(0);
 }
