d4e48f55e04a69e2f1b668c568f359845c88cece
Blue Espeon <mental1104@gmail.com>
2026-07-23 03:24:45 +0800

    kernel: route copyin through user alias window

--- kernel/vmcopyin.c
+++ kernel/vmcopyin.c
@@ -1,5 +1,6 @@
 #include "param.h"
 #include "types.h"
+#include "memlayout.h"
 #include "riscv.h"
 #include "defs.h"
 #include "spinlock.h"
@@ -30,14 +31,16 @@ int
 copyin_new(pagetable_t pagetable, char *dst, uint64 srcva, uint64 len)
 {
   struct proc *p = myproc();
-  if(p == 0 || pagetable != p->pagetable || srcva + len < srcva || srcva + len > p->sz)
+  if(p == 0 || pagetable != p->pagetable || srcva + len < srcva ||
+     srcva + len > p->sz || srcva + len > USERMAX)
     return -1;
 
+  // walkaddr() 负责物化 lazy page；u2kvmcopy() 随后会同步高地址别名。
   for(uint64 va = PGROUNDDOWN(srcva); va < srcva + len; va += PGSIZE)
     if(walkaddr(pagetable, va) == 0)
       return -1;
 
-  memmove(dst, (void*)srcva, len);
+  memmove(dst, (void*)KUSERADDR(srcva), len);
   stats.ncopyin++;
   return 0;
 }
@@ -50,19 +53,21 @@ int
 copyinstr_new(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
 {
   struct proc *p = myproc();
-  if(p == 0 || pagetable != p->pagetable || srcva >= p->sz)
+  if(p == 0 || pagetable != p->pagetable || srcva >= p->sz ||
+     srcva >= USERMAX)
     return -1;
 
   stats.ncopyinstr++;
   uint64 checked_page = MAXVA;
-  for(uint64 i = 0; i < max && srcva + i < p->sz; i++){
+  for(uint64 i = 0; i < max && srcva + i >= srcva &&
+      srcva + i < p->sz && srcva + i < USERMAX; i++){
     uint64 page = PGROUNDDOWN(srcva + i);
     if(page != checked_page){
       if(walkaddr(pagetable, page) == 0)
         return -1;
       checked_page = page;
     }
-    dst[i] = *(char*)(srcva + i);
+    dst[i] = *(char*)KUSERADDR(srcva + i);
     if(dst[i] == '\0')
       return 0;
   }
