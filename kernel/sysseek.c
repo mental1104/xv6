dfa2a0fada0fe3fb8cf6fdbad1012678fafcb7b3
Blue Espeon <mental1104@gmail.com>
2026-07-24 01:43:50 +0800

    fix: 补全 lseek 系统调用依赖

--- kernel/sysseek.c
+++ kernel/sysseek.c
@@ -1,7 +1,11 @@
 #include "types.h"
+#include "riscv.h"
 #include "defs.h"
 #include "param.h"
+#include "stat.h"
+#include "spinlock.h"
 #include "proc.h"
+#include "sleeplock.h"
 #include "file.h"
 
 /**
