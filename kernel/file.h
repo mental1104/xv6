da62c262685590fedd675bb58e10c80a587f1f07
Blue Espeon <mental1104@gmail.com>
2026-07-23 13:11:42 +0800

    fs: widen inode size and open-file offset

--- kernel/file.h
+++ kernel/file.h
@@ -5,13 +5,13 @@ struct file {
   char writable;
   struct pipe *pipe; // FD_PIPE
   struct inode *ip;  // FD_INODE and FD_DEVICE
-  uint off;          // FD_INODE
+  uint64 off;        // FD_INODE
   short major;       // FD_DEVICE
 };
 
 #define major(dev)  ((dev) >> 16 & 0xFFFF)
 #define minor(dev)  ((dev) & 0xFFFF)
-#define	mkdev(m,n)  ((uint)((m)<<16| (n)))
+#define mkdev(m,n)  ((uint)((m)<<16| (n)))
 
 // in-memory copy of an inode
 struct inode {
@@ -25,8 +25,8 @@ struct inode {
   short major;
   short minor;
   short nlink;
-  uint size;
-  uint addrs[NDIRECT+2];
+  uint64 size;
+  uint addrs[NDIRECT+NINDIRECT_LEVELS];
 };
 
 // map major device number to device functions.
