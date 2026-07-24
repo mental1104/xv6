6487bf3b60d3bd91928b908f9d1b6e3dd520f3d7
Blue Espeon <mental1104@gmail.com>
2026-07-25 01:06:14 +0800

    feat(memviz): name pid snapshot syscall

--- kernel/syscall_names.h
+++ kernel/syscall_names.h
@@ -49,6 +49,7 @@ static const char *const syscall_names[] = {
 [SYS_tcsetpgrp]        = "tcsetpgrp",
 [SYS_lseek]            = "lseek",
 [SYS_execve]           = "execve",
+[SYS_memsnapshot_pid]  = "memsnapshot_pid",
 };
 
 // 名称表中可访问的元素数量（包含下标 0），用于限制遍历和查找范围。
