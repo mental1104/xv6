a7e48ec15e81f8f082acbf706339efc8912ef36f
Blue Espeon <mental1104@gmail.com>
2026-07-26 00:46:11 +0800

    chore: 同步并发基线系统调用编号

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -46,4 +46,5 @@
 #define SYS_getprocs         45
 #define SYS_swapout          46
 #define SYS_swapinfo         47
-#define SYS_pollread         48
+#define SYS_concurrencylab   48
+#define SYS_pollread         49
