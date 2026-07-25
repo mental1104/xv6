1baa118ece7762bca89730ea1d7de9d938e07201
Blue Espeon <mental1104@gmail.com>
2026-07-26 00:12:15 +0800

    feat: 声明 pipe 就绪等待接口

--- kernel/defs.h
+++ kernel/defs.h
@@ -85,6 +85,12 @@ int             pipealloc(struct file**, struct file**);
 void            pipeclose(struct pipe*, int);
 int             piperead(struct pipe*, uint64, int);
 int             pipewrite(struct pipe*, uint64, int);
+int             pipereadable(struct pipe*);
+
+// poll.c
+void            pollinit(void);
+void            pollnotify(void);
+int             pollreadfiles(struct file**, int, int);
 
 // printf.c
 void            printf(char*, ...);
