ee6a4645259bb76c431646aff8ffdc40e96a0a14
Blue Espeon <mental1104@gmail.com>
2026-07-25 23:41:47 +0800

    feat: 补充 locklab 系统调用名称

--- kernel/syscall_names.h
+++ kernel/syscall_names.h
@@ -50,6 +50,7 @@ static const char *const syscall_names[] = {
 [SYS_lseek]            = "lseek",
 [SYS_execve]           = "execve",
 [SYS_getprocs]         = "getprocs",
+[SYS_locklab]          = "locklab",
 };
 
 // 名称表中可访问的元素数量（包含下标 0），用于限制遍历和查找范围。
