0532199d9ded88eafc4ca3a75b13aca1aa22ab50
Blue Espeon <mental1104@gmail.com>
2026-07-25 21:18:28 +0800

    chore(vm): retain latest page-table test layout entry

--- user/init.c
+++ user/init.c
@@ -78,6 +78,7 @@ static struct image_file image_files[] = {
   {"/xv6test", XV6_USR_BIN_PATH("xv6test")},
 
   {"/memviztest", XV6_TEST_PATH("memviztest")},
+  {"/pgtbltest", XV6_TEST_PATH("pgtbltest")},
   {"/vaaccesstest", XV6_TEST_PATH("vaaccesstest")},
   {"/addresswindowtest", XV6_TEST_PATH("addresswindowtest")},
   {"/forktest", XV6_TEST_PATH("forktest")},
