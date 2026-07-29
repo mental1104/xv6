bd17bc92169b7a90c10a07184babc482b7587276
Blue Espeon <mental1104@gmail.com>
2026-07-30 00:55:16 +0800

    chore: use docker compose for xv6 environment management

--- docker.mk
+++ docker.mk
@@ -1,20 +1,21 @@
 DOCKER ?= docker
+DOCKER_COMPOSE ?= $(DOCKER) compose
 DOCKER_IMAGE ?= xv6-dev
 
 # 构建包含当前源码、RISC-V 工具链和 QEMU 的可复现 xv6 镜像。
 docker-build:
-	$(DOCKER) build --pull -t $(DOCKER_IMAGE) .
+	$(DOCKER_COMPOSE) build
 
-# 使用交互式串口启动镜像内的 QEMU；退出 QEMU 时容器随之删除。
-docker-qemu: docker-build
-	$(DOCKER) run --rm -it $(DOCKER_IMAGE)
+# 使用 docker compose 管理 QEMU 参数、资源限制和交互终端。
+docker-qemu:
+	$(DOCKER_COMPOSE) run --rm xv6
 
 # 进入镜像内的源码目录，便于复验工具链和手工执行构建命令。
-docker-shell: docker-build
-	$(DOCKER) run --rm -it --entrypoint /bin/bash $(DOCKER_IMAGE)
+docker-shell:
+	$(DOCKER_COMPOSE) run --rm --entrypoint /bin/bash xv6
 
 # 在镜像内执行仓库现有的默认回归入口，并透传测试退出码。
-docker-test: docker-build
-	$(DOCKER) run --rm $(DOCKER_IMAGE) make test
+docker-test:
+	$(DOCKER_COMPOSE) run --rm test
 
 .PHONY: docker-build docker-qemu docker-shell docker-test
